edb1ef325130ac99de8e2dddf78ec9c57fcf5235
Esma Toksoy <esma.toksoy@std.antalya.edu.tr>
2025-10-23 12:45:44 +0300

    init commit

---
+++ src/test/java/com/esma/reservation/manager/ReservationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esma.reservation.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
